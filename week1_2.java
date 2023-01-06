193723d9ff74169840eb5dc4c219ae95b775a318
Gunzolid <112106347+Gunzolid@users.noreply.github.com>
2023-01-06 15:15:49 +0700

    Update week1_2.java

--- week1_2.java
+++ week1_2.java
@@ -5,3 +5,27 @@ public class Main {
         System.out.print("hello");
     }
 }
+
+public class ch2_1 {
+    public static void main(String[] args) {
+        int Num = 10;
+        float Float = 1.23456f;
+        char Letter = 'A';
+        String Text = "Hello World";
+        System.out.println("My Number is " + Num);
+        System.out.println("My Float is " + Float);
+        System.out.println("My Letter is " + Letter);
+        System.out.println("My Letter is " + (int)Letter);
+        System.out.println("My Text is " + Text);
+        System.out.println("My Text is " + Text + " Java");
+        Text = "Java";
+        System.out.println("My Text is " + Text);
+        System.out.println("My Text is " + Text + Num);
+        System.out.println("My Text is " + Text + Num);
+      
+        System.out.println("My Num + 100 = " + (Num + 100));
+        // + คือการต่อข้อความ
+        System.out.println("My Num + 100 = " + Num + 100);
+        // + ในวงเล็บคือกระบวกการ
+    }
+}
