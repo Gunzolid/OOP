06c3ea6180b15258dfb0a7ce2113875a174c3738
Gunzolid <112106347+Gunzolid@users.noreply.github.com>
2023-01-24 14:03:49 +0700

    Update Week3_2.java

--- Week3_2.java
+++ Week3_2.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Week4;
+package Week3;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -13,15 +13,17 @@ import java.util.Scanner;
  *
  * @author coc
  */
-public class Week3_2 {
+public class Week3_2_Homework {
     public static void main(String[] args) {
-  
+  /*
         int divide = 8;
         for (int i = 1; i <= 500; i++){
             if( i % divide == 0){
                 System.out.printf("%d, ", i);
             }
         }
+    */    
+        
 //        Homework 3-1 Natasan Chantip 6530611021
         Scanner Num = new Scanner(System.in);
         Scanner Net = new Scanner(System.in);
@@ -93,14 +95,78 @@ public class Week3_2 {
             Scanner year = new Scanner(System.in);
             
             System.out.print("Enter amount: ");
-            float money = cash.nextFloat();
+            float C = cash.nextFloat();
             
             System.out.print("Enter interest rate: ");
-            int r = rate.nextInt();
+            float r = rate.nextFloat();
             
             System.out.print("Enter period in yeras: ");
-    }
+            int y = year.nextInt();
             
+            
+            float Interest = 0;
+            float Amount = C + Interest;
+            float All = 0; 
+            System.out.println("Year              Amount                Interest");
+            
+            for(int i = 1; i <= y; i++){
+                System.out.printf("%3d       ",i );
+                System.out.printf("%,15.2f       ",Amount += Interest);
+                System.out.printf("%,15.2f \n",Interest = Amount*(r/100));
+                All += Interest; 
+                
+                if(i == y){
+                    System.out.printf("Total Interest: %,.2f \n",  All);
+                }
+            }
+            
+//            Homework 3-4
+            Scanner in_time_h = new Scanner(System.in);
+            Scanner in_time_m = new Scanner(System.in);
+            Scanner out_time_h = new Scanner(System.in);
+            Scanner out_time_m = new Scanner(System.in);
+            
+            System.out.print("In time hour: ");
+            int in_h = in_time_h.nextInt();
+            
+            System.out.print("In time minute: ");
+            int in_m = in_time_m.nextInt();
+            
+            System.out.print("Out time hour: ");
+            int out_h = out_time_h.nextInt();
+            
+            System.out.print("Out time minute: ");
+            int out_m = out_time_m.nextInt();
+            
+            int in_h_m = (int)(in_h * 60);
+            int out_h_m = (int)(out_h * 60);
+            
+            int Allin = in_h_m + in_m;
+            int Allout = out_h_m + out_m; 
+            int time = Allout - Allin;
+            
+            if(time <= 15 ){
+                int parking_charges = 0;
+                System.out.println("==================");
+                System.out.println("Parking charges: " + parking_charges);
+
+            }
+            else if(time > 15 && time <= 180){
+                int parking_charges = 10;
+                System.out.println("==================");
+                System.out.println("Parking charges: " + parking_charges);
+            }
+            else if(time > 180 && time <= 360){
+                int parking_charges = 20 + 10;
+                System.out.println("==================");
+                System.out.println("Parking charges: " + parking_charges);
+            }
+            else{
+                int parking_charges = 200;
+                System.out.println("==================");
+                System.out.println("Parking charges: " + parking_charges);
+            }
+    }
                         }
         
    
